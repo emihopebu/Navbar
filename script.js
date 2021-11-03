73b913087acdceaa6081c342d154b344a158bc0e
emihopebu <emihopebu@gmail.com>
2021-11-03 18:06:55 -0500

    Adding files

--- script.js
+++ script.js
@@ -6,5 +6,8 @@ links.forEach((link) => {
       link.className = "navbar-link";
     });
     link.classList.add("change");
+
+    link.previousElementSibling.classList.add("prevElChange");
+    link.nextElementSibling.classList.add("nextElChange");
   });
 });
